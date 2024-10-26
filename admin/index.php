e41c7a281e8ede176bcc29bc8e34af5a6904284a
Danginishit <danginishit114@gmail.com>
2024-10-26 23:04:09 +0530

    added new favicon icon

--- admin/index.php
+++ admin/index.php
@@ -14,6 +14,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Admin | Login</title>
+        <link rel="icon" href="images/favicon.png" type="image/x-icon">
         <link rel="stylesheet" href="../css/bootstrap.min.css" />
         <link rel="stylesheet" href="font/font-awesome-4.7.0/css/font-awesome.css">
         <link rel="stylesheet" href="../css/style.css">
